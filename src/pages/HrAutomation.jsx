17a64c687d6e7dbb04f170ec7554fc435b90188f
kite335 <kiteflying335@gmail.com>
2025-12-13 11:52:23 +0000

    mobile site hr pages

--- src/pages/HrAutomation.jsx
+++ src/pages/HrAutomation.jsx
@@ -4,6 +4,10 @@ import ProductTop from '../components/ProductTop'
 import hrImg from '../assets/product/hr.png'
 import hrImg2 from '../assets/product/hr2.png'
 import hrImg3 from '../assets/product/hr3.png'
+import hrImg4 from '../assets/product/hr4.jpg'
+import hrImg5 from '../assets/product/hr5.jpg'
+import hrImg6 from '../assets/product/hr6.jpg'
+
 const HrAutomation = () => {
     const hrData = {
         h: "HR management & workflow automation",
@@ -14,7 +18,22 @@ const HrAutomation = () => {
         h_3: "Workflow automation",
         p_3: "Stop wasting hours on repetitive tasks. With Bitrix24, you can automate routine HR workflow (like leave requests, approvals, and document signing) so they run seamlessly in the background. This means fewer delays, fewer errors, and more time for your team to focus on meaningful work.",
         img_3:hrImg3,
-        btn:"AUTOMATE WORKFLOWS WITH BITIX24"
+        btn:"AUTOMATE WORKFLOWS WITH BITIX24",
+
+        h_4: "Employee management",
+        p_4: "Bitrix24 puts everything you need to manage people and teams in one easy-to-use platform. From schedules and time tracking to collaboration and access permissions, you can keep everyone organized without switching between multiple apps. The result: faster decisions, fewer bottlenecks, and a smoother employee experience.",
+        img_4:hrImg4,
+        btn_1:"MANAGE YOUR TEAM WITH BITIX24",
+
+        h_5: "AI-powered workspace",
+        p_5: " Our AI assistant CoPilot goes beyond simple automation. It helps your team brainstorm, write, and generate ideas, while also analyzing meetings and suggesting actionable improvements. Whether you’re drafting HR policies, preparing reports, or refining team communications, CoPilot saves time and adds intelligence to everyday tasks.",
+        img_5:hrImg5,
+        btn_2:"TRY COPILOT IN BITIX24",
+
+        h_6: "More than just HR & automation",
+        p_6: "In addition to various HR management and workflow automation tools, Bitrix24 features CRM, task management, chat, video calls, file storage, calendar, online documents, and more.",
+        img_6:hrImg6,
+        
     }
     return (
         <>
--- src/pages/Mobile.jsx
+++ src/pages/Mobile.jsx
@@ -4,15 +4,35 @@ import crmImage2 from '../assets/product/crm2.jpg'
 import crmImage3 from '../assets/product/crm3.jpg'
 
 const mobileData={
-        h: "Free CRM software for your business",
-        p: "Sell more. Sell better. Sell faster with Bitrix24 a single platform to cover all your needs.",
-        img: crmImage, h_2: "CRM that's packed with sales, marketing, and analytics tools",
-        p_2: "Bitrix24 CRM has everything you need to acquire leads, turn them into deals, receive payments, manage inventory, e-sign documents, run marketing campaigns, increase customer engagement, grow customer retention, track sales metrics, and more.",
-        img_2: crmImage2,
-        h_3: "Serious automation capabilities",
-        p_3: "Set up automated client notifications via SMS or email, use our customizable triggers to automatically move leads/deals down the sales pipeline, configure automated notifications to your sales reps based on the lead/deal status.",
-        img_3: crmImage3,
-        btn: "AUTOMATE YOUR SALES"
+        h: "Mobile communications",
+        p: "Stay in touch with your team and on top of your projects with the Bitrix24 mobile app. Featuring tasks, team messenger, video calls, calendar, and other tools, this is the ultimate means of collaboration and communication.",
+        img: "https://www.bitrix24.in/upload/optimizer/converted/images/content_en/tools/communications/mobile-communications/header-human-mobile-communications.1366w.png.webp?1764863685915", 
+        h_2: "Messenger",
+
+        p_2: "Our messenger is a secure way of keeping all your team communications inside the company. Create chats and channels, communicate with external users (freelancers, contractors) via Collab, share files and give reactions.",
+
+        img_2:"https://www.bitrix24.in/upload/optimizer/converted/images/content_en/tools/communications/mobile-communications/messenger.png.webp?1764863685915",
+
+        h_3: "Comments",
+
+        p_3: "Comments in the Bitrix24 mobile messaging app are a quick and easy way to discuss work-related issues and share important information with your team. Likes, reactions, and quotes add a bit of human touch to the usual office communication routine.",
+        img_3: "https://www.bitrix24.in/upload/optimizer/converted/images/content_en/tools/communications/mobile-communications/comments.png.webp?1764863685915",
+        btn: "AUTOMATE YOUR SALES",
+
+          h_4:"Video calls",
+          p_4:"Why spend time exchanging messages when you can make a quick video call? Resolving work issues and discussing new ideas is always easier that way. Start an online meeting right from a chat, an event in your calendar, or directly from any task in our mobile app.",
+          img_4:"https://www.bitrix24.in/upload/optimizer/converted/images/content_en/tools/communications/mobile-communications/video-calls.png.webp?1764863685915",
+           btn_1:"START FOR FREE",
+
+          h_5:"Channels",
+          p_5:"Create a channel for company announcements, employee onboarding, or industry news to share information in a way that’s both organic and engaging.",
+          img_5:"https://www.bitrix24.in/upload/optimizer/converted/images/content_en/tools/communications/mobile-communications/messenger-card-2.png.webp?1764863685915",
+           btn_2:"GET STARTED",
+          h_6:"More than just online workspace",
+          p_6:"There's so much you can do with a simple event in Bitrix24 Calendar: create a quick chat, call up a client right from a deal/lead, set up an online meeting, assign a task, and more.",
+          img_6:"https://www.bitrix24.in/upload/optimizer/converted/images/content_en/screens/tools/crm/features_clear.png.webp?1764863685915"
+
+
     }
 
 const Mobile=()=>{
